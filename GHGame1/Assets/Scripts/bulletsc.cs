2f120ed6aa5eaed73a0e1ee7a8f96331544112d8
Rias Ryuzaki <lukasz.szturemski@gmail.com>
2021-07-09 21:12:18 +0200

    space game test

---
+++ GHGame1/Assets/Scripts/bulletsc.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletsc : MonoBehaviour
+{
+    float speed = 10f;
+    shipGame control;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GameObject.Find("GameControl").GetComponent<shipGame>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+        control.AddScore();
+        Destroy(this.gameObject);
+    }
+}
